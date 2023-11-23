9de428c63e80b516c6fe1476884840fffe1d7b8f
duckzuybidan <ducduy19012310@gmail.com>
2023-11-23 22:22:55 +0700

    .

--- app/api/meme/quickUpload/route.ts
+++ app/api/meme/quickUpload/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server"
 import ytdl from "@distube/ytdl-core"
 import fs from 'fs'
 import connectDB from '@/lib/connectDB'
+export const revalidate = 5
 const ytDownload = (url: string) => {
     return new Promise((resolve, reject) => {
         try{
