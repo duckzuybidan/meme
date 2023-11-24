561ce0445632957844553f2145b904d1309ddfcc
duckzuybidan <ducduy19012310@gmail.com>
2023-11-25 00:50:42 +0700

    /

--- app/api/meme/quickUpload/getFileString/route.ts
+++ app/api/meme/quickUpload/getFileString/route.ts
@@ -1,7 +1,14 @@
 import { NextRequest, NextResponse } from "next/server"
 import ytdl from "ytdl-core"
 export const revalidate = 5
-const getFileString = async (url: string) => {
+import { v2 as cloudinary } from "cloudinary"
+
+cloudinary.config({
+  cloud_name: "dcalppd08",
+  api_key: "521941113518647",
+  api_secret: "P18AZVui5k7-GHHDKnXXOi8W7xU",
+})
+const ytDownload = async (url: string) => {
   try {
     const chunks: any[] = []
     const stream = ytdl(url)
@@ -10,7 +17,15 @@ const getFileString = async (url: string) => {
     }
     const buffer = Buffer.concat(chunks)
     const fileString = buffer.toString("base64")
-    return fileString
+    await cloudinary.uploader.upload(
+      `data:video/mp4;base64,${fileString}`,
+      {
+        resource_type: "auto",
+        public_id: `${new Date().getTime()}`,
+        folder: "memes",
+        format: "mp4",
+      }
+    )
   } 
   catch (error) {
     throw new Error(error as any)
@@ -20,8 +35,8 @@ const getFileString = async (url: string) => {
 export async function GET(req: NextRequest) {
   const url = req.nextUrl.searchParams.get('url') || ''
   try {
-    const fileString = await getFileString(url)
-    return NextResponse.json({ data: fileString })
+    await ytDownload(url)
+    return NextResponse.json({ data: 'pass' })
   } 
   catch (error) {
     return NextResponse.json({ error: new Error(error as any).message })
