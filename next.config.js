cfbd86105e71eee6f32010972f315ae3cddf7296
duckzuybidan <ducduy19012310@gmail.com>
2023-11-24 18:52:36 +0700

    .

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { 
-    
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: '/api/:path*',
+            apiTimeout: 300,
+          },
+        ]
+    },
 }
 
 module.exports = nextConfig
