5db5af79fec957c4740ff35627ed904660d390e9
duckzuybidan <ducduy19012310@gmail.com>
2023-11-24 18:24:41 +0700

    .

--- app/api/meme/quickUpload/route.ts
+++ app/api/meme/quickUpload/route.ts
@@ -1,58 +1,63 @@
 import { NextRequest, NextResponse } from "next/server"
 import ytdl from "ytdl-core"
-import fs from 'fs'
-import connectDB from '@/lib/connectDB'
-import {v2 as cloudinary} from 'cloudinary';
+import connectDB from "@/lib/connectDB"
+import { v2 as cloudinary } from "cloudinary"
+
 cloudinary.config({
-    cloud_name: 'dcalppd08',
-    api_key: '521941113518647',
-    api_secret: 'P18AZVui5k7-GHHDKnXXOi8W7xU'
-  });
+  cloud_name: "dcalppd08",
+  api_key: "521941113518647",
+  api_secret: "P18AZVui5k7-GHHDKnXXOi8W7xU",
+})
+
 const ytDownload = (url: string) => {
-    return new Promise((resolve, reject) => {
-        try{
-            ytdl(url).pipe(fs.createWriteStream('video.mp4')).on('finish', () => {
-                const fileBuffer  = fs.readFileSync('video.mp4')
-                const fileString = fileBuffer.toString('base64');
-                cloudinary.uploader.upload(
-                    `data:video/mp4;base64,${fileString}`,
-                    {
-                    resource_type: 'auto',
-                      public_id: `${new Date().getTime()}`, 
-                      folder: 'memes',      
-                      format: 'mp4',                
-                    },
-                    (error, result) => {
-                      if (error) {
-                        reject(error)
-                      } 
-                      else {
-                        fs.unlinkSync('video.mp4')
-                        resolve(result?.url)
-                      }
-                    }
-                  )
-            })
-        }
-        catch(error){
-            reject(error)
-        }
-    }) 
-}
-export async function POST(req: NextRequest) {
-    await connectDB()
-    const formData = await req.json() 
-    try{
-        const res = await ytDownload(formData.url)
-        .then(url => {
-            return url
+  return new Promise((resolve, reject) => {
+    try {
+      const chunks: any[] = []
+
+      ytdl(url)
+        .on("data", (chunk) => {
+          chunks.push(chunk)
         })
-        .catch(error => {
-            throw new Error(error)
+        .on("end", () => {
+          const buffer = Buffer.concat(chunks)
+          const fileString = buffer.toString("base64")
+
+          cloudinary.uploader.upload(
+            `data:video/mp4;base64,${fileString}`,
+            {
+              resource_type: "auto",
+              public_id: `${new Date().getTime()}`,
+              folder: "memes",
+              format: "mp4",
+            },
+            (error, result) => {
+              if (error) {
+                reject(error)
+              } else {
+                resolve(result?.url)
+              }
+            }
+          )
         })
-        return NextResponse.json({data: res})
+        .on("error", (error) => {
+          reject(error)
+        })
+    } catch (error) {
+      reject(error)
     }
-    catch(error){
-        return NextResponse.json({error: new Error(error as any).message})
-    }   
+  })
+}
+
+export async function POST(req: NextRequest) {
+  await connectDB()
+
+  const formData = await req.json()
+
+  try {
+    const res = await ytDownload(formData.url)
+
+    return NextResponse.json({ data: res })
+  } catch (error) {
+    return NextResponse.json({ error: new Error(error as any).message })
+  }
 }
