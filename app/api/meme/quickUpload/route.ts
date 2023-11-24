56b79ec617c606905645310cf0b18658d3d6d2bb
duckzuybidan <ducduy19012310@gmail.com>
2023-11-24 11:20:16 +0700

    .

--- app/api/meme/quickUpload/route.ts
+++ app/api/meme/quickUpload/route.ts
@@ -2,11 +2,11 @@ import { NextRequest, NextResponse } from "next/server"
 import ytdl from "ytdl-core"
 import fs from 'fs'
 import connectDB from '@/lib/connectDB'
-import admin from 'firebase-admin'
+import path from "path"
 const ytDownload = (url: string) => {
     return new Promise((resolve, reject) => {
         try{
-            ytdl(url).pipe(fs.createWriteStream(process.cwd() + '/public/video.mp4')).on('finish', async () => {
+            ytdl(url).pipe(fs.createWriteStream(path.join(process.cwd(), '/public/video'))).on('finish', async () => {
                 
                 resolve('Download success')
             })
@@ -20,14 +20,14 @@ const ytDownload = (url: string) => {
 export async function POST(req: NextRequest) {
     await connectDB()
     const formData = await req.json()
-    const path = process.cwd() + '/public/video.mp4'
+    const p = path.join(process.cwd(), '/public/video')
     try{
         await ytDownload(formData.url)
         .then(message => console.log(message))
         .catch(error => {
             throw new Error(error)
         })
-        return NextResponse.json({message: path})
+        return NextResponse.json({message: p})
     }
     catch(error){
         return NextResponse.json({error: new Error(error as any).message})
