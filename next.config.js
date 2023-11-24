0e497d4b1b632e2931837ad40119a6fa0015d7ae
duckzuybidan <ducduy19012310@gmail.com>
2023-11-24 19:15:11 +0700

    .

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { 
-    serverOptions: {
-        // Increase the timeout for all API routes to 5 minutes (300 seconds)
-        timeout: 300000,
-      },
+  
 }
 
 module.exports = nextConfig
