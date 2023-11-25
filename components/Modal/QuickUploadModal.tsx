1d7eada5b48efa60b53136de8d3d403157bdb737
duckzuybidan <ducduy19012310@gmail.com>
2023-11-25 16:11:29 +0700

    .

--- components/Modal/QuickUploadModal.tsx
+++ components/Modal/QuickUploadModal.tsx
@@ -18,7 +18,7 @@ export default function QuickUploadModal({modal, onClose}: {modal: quickUploadMo
             revalidate: 5
           }
           })
-          .then(res => res.json())
+          .then(res => res.clone())
           .then(async (res) => {
             const isJson = res.headers.get('Content-Type')?.includes('application/json')
             if(isJson){
